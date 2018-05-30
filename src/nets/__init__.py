972e1a2907d24dacaf3f7ba26c29a1ef119a0034
Diganth P <jayanth.jayanth.prakash@gmail.com>
2018-05-30 22:20:13 +0530

    Add files via upload

---
+++ src/nets/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from .squeezeDet import SqueezeDet
+from .squeezeDetPlus import SqueezeDetPlus
+from .resnet50_convDet import ResNet50ConvDet
+from .vgg16_convDet import VGG16ConvDet
